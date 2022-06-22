62e607f757670f09bdea7730ca624660276aa0b3
Nikolay <nikolay_git@rambler.ru>
2022-06-22 20:14:18 +0500

    Создали программу с ветвлением

---
+++ Lesson_1/005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(UserName);
+}

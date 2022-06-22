eca57845e0d416a2d0bae59448a10e1336b67ab3
Nikolay <nikolay_git@rambler.ru>
2022-06-22 20:07:26 +0500

    Добавили генерацию складываемых чисел

--- Lesson_1/003_Sum/Program.cs
+++ Lesson_1/003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.Write(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write(result);

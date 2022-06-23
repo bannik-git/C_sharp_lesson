d4645452faade6da5c64c5364d61da9e513f6343
Nikolay <nikolay_git@rambler.ru>
2022-06-23 22:55:24 +0500

    Создан блок находящий индекс максимального и минимального элемента массива

---
+++ Lesson_1/009_SumBetweenMaxAndMin/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] Numbers = new int[] {2, 4, 5, 7, 12, 14, 6, 3, 1};
+
+int Size = Numbers.Length,
+    index = 0;
+
+int MaxNumber = Numbers[index],
+    MinNumber = Numbers[index],
+    MaxNumberIndex = index,
+    MinNumberIndex = index;
+
+while(index < Size)
+{
+    if (MaxNumber < Numbers[index])
+    {
+        MaxNumber = Numbers[index];
+        MaxNumberIndex = index;
+    }
+    else if (MinNumber > Numbers[index])
+    {
+        MinNumber = Numbers[index];
+        MinNumberIndex = index;
+    }
+
+    index++;
+}   
+
+Console.WriteLine("Индекс максимального элемента - " + MaxNumberIndex + ", " + "индекс минимального элемента - " + MinNumberIndex);

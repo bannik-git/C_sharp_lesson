266dc0fe100408480ee30add104f6eab53e5df4b
Nikolay <nikolay_git@rambler.ru>
2022-06-23 23:12:03 +0500

    Добавил функуцию считающую сумму элементов между максимальным и минимальным элементами массива

--- Lesson_1/009_SumBetweenMaxAndMin/Program.cs
+++ Lesson_1/009_SumBetweenMaxAndMin/Program.cs
@@ -1,4 +1,16 @@
-﻿int[] Numbers = new int[] {2, 4, 5, 7, 12, 14, 6, 3, 1};
+﻿int SumBetweenMaxAndMin(int[] UserNumbers, int StartIndex, int EndIndex)
+{
+    int result = 0;
+    while(StartIndex < EndIndex)
+    {
+        result = result + UserNumbers[StartIndex];
+        StartIndex++;
+    }
+    return result;
+}
+
+
+int[] Numbers = new int[] {2, 4, 5, 7, 12, 14, 6, 3, 1};
 
 int Size = Numbers.Length,
     index = 0;
@@ -24,4 +36,11 @@ while(index < Size)
     index++;
 }   
 
-Console.WriteLine("Индекс максимального элемента - " + MaxNumberIndex + ", " + "индекс минимального элемента - " + MinNumberIndex);
+if (MinNumberIndex < MaxNumberIndex) 
+{
+    Console.WriteLine(SumBetweenMaxAndMin(Numbers, MinNumberIndex + 1, MaxNumberIndex));
+}
+else
+{
+    Console.WriteLine(SumBetweenMaxAndMin(Numbers, MaxNumberIndex + 1, MinNumberIndex));
+}

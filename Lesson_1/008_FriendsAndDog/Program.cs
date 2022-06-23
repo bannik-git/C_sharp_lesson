1944477496e66c7b9e11ae5e4177e206c7216254
Nikolay <nikolay_git@rambler.ru>
2022-06-23 21:14:04 +0500

    Создание программы из лекции №1 с друзьями и собакой

---
+++ Lesson_1/008_FriendsAndDog/Program.cs
@@ -0,0 +1,31 @@
+﻿int FirstFriendsSpeed = 2,
+    SecondFriendsSpeed = 3,
+    DogSpeed = 5,
+    count = 0;
+
+double distance = 10000, time = 0;
+
+int DirectionDog = 1; // 1 - от первого ко второму, 2 - от второго к первому
+
+while (distance > 2)
+{   
+        
+    if (DirectionDog == 1)
+    {
+        time = distance / (SecondFriendsSpeed + DogSpeed);
+        DirectionDog = 2;
+    }
+    else
+    {
+        time = distance / (FirstFriendsSpeed + DogSpeed);
+        DirectionDog = 1;
+    }
+
+    distance = distance - (FirstFriendsSpeed + SecondFriendsSpeed) * time;
+    count++;
+}
+
+Console.WriteLine(count);
+
+
+

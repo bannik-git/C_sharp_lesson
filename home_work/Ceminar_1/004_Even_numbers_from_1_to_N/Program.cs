9666d18669de520167e9383b46678c861adfa60e
Nikolay <nikolay_git@rambler.ru>
2022-06-25 22:39:45 +0500

    Программа задания 4

---
+++ home_work/Ceminar_1/004_Even_numbers_from_1_to_N/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите число больше 1: ");
+int userNumber = Convert.ToInt32(Console.ReadLine()),
+    count = 1;
+
+if (userNumber < 1)
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+else if (userNumber == 1)
+{
+    Console.WriteLine("Четных чисел нет");
+}
+else
+{
+    while (count <= userNumber)
+    {
+        if (count%2 == 0)
+        {
+            if ((userNumber - count) == 1)
+            {
+                Console.Write(count);
+                count+=2;
+            }
+            else if ((userNumber - count) == 0)
+            {
+                Console.Write(count);
+                count+=2;
+            }
+            else
+            {
+                Console.Write(count + ", ");
+                count+=2;                
+            }
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+}

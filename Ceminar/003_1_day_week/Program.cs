0d4e04439994a17a20b3357321e6fd4de9c9ca5c
Nikolay <nikolay_git@rambler.ru>
2022-06-25 14:16:27 +0500

    Новоя реализация нахождения дня недели

---
+++ Ceminar/003_1_day_week/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите номер дня недели (1-7): ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string [] days = new string  []{"Понедельник", "Вторник", "Среда", "Четверг", "Пянтица", "Суббота", "Воскресенье"};
+
+Console.WriteLine(days[a-1]);

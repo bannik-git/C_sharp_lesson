d5d61274fa14354ab885feb033ab3fb90e15596c
Nikolay <nikolay_git@rambler.ru>
2022-06-25 22:38:57 +0500

    Программа задания 1

---
+++ home_work/Ceminar_1/001_max_min/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число №1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("max = " + number1);
+}
+else if (number1 == number2)
+{
+    Console.WriteLine("max = " + number1);
+}
+else
+{
+    Console.WriteLine("max = " + number2);
+}

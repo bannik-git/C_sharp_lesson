65bef2b7c36cc96aa20c98a99625f73906d3439c
Nikolay <nikolay_git@rambler.ru>
2022-06-25 13:22:57 +0500

    Программа поверяющая не является ли число 1 квадратом числа 2

---
+++ Ceminar/002_quadro_i/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число 1 ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a*a == b) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}

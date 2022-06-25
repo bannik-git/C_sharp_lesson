61b118655a27d5c6fdfb0a643413af9181bf5023
Nikolay <nikolay_git@rambler.ru>
2022-06-25 13:23:35 +0500

    Программа находящая день недели

---
+++ Ceminar/003_day_week/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите номер дня недели ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (a == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (a == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (a == 5)
+{
+    Console.WriteLine("Пятнца");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("В неделе только 7 дней");
+}

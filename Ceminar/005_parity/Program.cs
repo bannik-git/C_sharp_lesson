5ccfde88fcba072a2c0090f854c7492f584683cf
Nikolay <nikolay_git@rambler.ru>
2022-06-25 14:19:00 +0500

    Программа вывода последнего числа в трех значном числе

---
+++ Ceminar/005_parity/Program.cs
@@ -0,0 +1,19 @@
+﻿bool answer = true;
+while (answer)
+{
+Console.Write("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine()); // 456
+int lastNumber = userNumber % 10;
+
+
+    if (userNumber > 99 & userNumber < 1000) // userNumber: [100:999]
+    {
+        Console.Write(lastNumber);
+        answer = false;
+    }
+    else
+    {
+    Console.WriteLine("Вы ввели не трехзначное число.");
+  
+    }
+}

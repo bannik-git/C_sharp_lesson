df60dd933d586cee77e98c65d388388affbaffe4
Nikolay <nikolay_git@rambler.ru>
2022-06-25 14:18:27 +0500

    Программа вывода всех чисел от -N до N включительно

---
+++ Ceminar/004_numders_between_N/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число больше 0: ");
+int userNumber = Convert.ToInt32(Console.ReadLine()),
+    count = - userNumber;
+
+while (count <= userNumber)
+{
+    Console.Write(count++ + ", ");
+}

fa5bd8a1e960fa8a1313dd833f221926b62303e9
Nikolay <nikolay_git@rambler.ru>
2022-06-25 22:39:33 +0500

    Программа задания 3

---
+++ home_work/Ceminar_1/003_Even_parity/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber % 2 ==0)
+{
+    Console.WriteLine("Число " + userNumber + " четное.");
+}
+else
+{
+    Console.WriteLine("Число " + userNumber + " нечетное.");
+}

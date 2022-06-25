f924a29e2836f0f795cf4ceef9644f559a2f2fea
Nikolay <nikolay_git@rambler.ru>
2022-06-25 12:24:33 +0500

    Поправил название переменных

--- Lesson_1/008_FriendsAndDog/Program.cs
+++ Lesson_1/008_FriendsAndDog/Program.cs
@@ -1,27 +1,27 @@
-﻿int FirstFriendsSpeed = 2,
-    SecondFriendsSpeed = 3,
-    DogSpeed = 5,
+﻿int firstFriendsSpeed = 2,
+    secondFriendsSpeed = 3,
+    dogSpeed = 5,
     count = 0;
 
 double distance = 10000, time = 0;
 
-int DirectionDog = 1; // 1 - от первого ко второму, 2 - от второго к первому
+int directionDog = 1; // 1 - от первого ко второму, 2 - от второго к первому
 
 while (distance > 2)
 {   
         
-    if (DirectionDog == 1)
+    if (directionDog == 1)
     {
-        time = distance / (SecondFriendsSpeed + DogSpeed);
-        DirectionDog = 2;
+        time = distance / (secondFriendsSpeed + dogSpeed);
+        directionDog = 2;
     }
     else
     {
-        time = distance / (FirstFriendsSpeed + DogSpeed);
+        time = distance / (firstFriendsSpeed + dogSpeed);
         DirectionDog = 1;
     }
 
-    distance = distance - (FirstFriendsSpeed + SecondFriendsSpeed) * time;
+    distance = distance - (firstFriendsSpeed + secondFriendsSpeed) * time;
     count++;
 }
 

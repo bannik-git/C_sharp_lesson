cce9ec4421c0638348705f52a467f68ec3a751fc
Nikolay <nikolay_git@rambler.ru>
2022-06-26 18:36:03 +0500

    Семинар 2 задание 3

---
+++ home_work/Ceminar_2/003_day_off/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите цифру дня недели (1-7): ");
+int userDay = Convert.ToInt32(Console.ReadLine());
+
+int saturday = 6,
+    sunday = 7;
+
+if (userDay == saturday || userDay == sunday)
+{
+    Console.WriteLine("Выходной день.");
+}
+else if (userDay > 7)
+{
+    Console.WriteLine("В неделе только 7 дней!");
+}
+else
+{
+    Console.WriteLine("Не выходной день.");
+}
+
+

884c5959a8d0aa25e309a365d2d4eeb6738b678c
Nikolay <nikolay_git@rambler.ru>
2022-06-26 13:02:40 +0500

    Программа проверки двух чисел на кратность

---
+++ Ceminar_2/004_Multiplicity_of_division/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число 1: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+// int randomNumber = new Random().Next(100, 1000);
+// int randomNumber = new Random().Next(100, 1000);
+
+if (userNumber1 % userNumber2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {userNumber1 % userNumber2}");
+}

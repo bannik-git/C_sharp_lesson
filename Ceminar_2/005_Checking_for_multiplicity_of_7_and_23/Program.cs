d607e108bb31911c59a6f16137fae8ab9c66ada5
Nikolay <nikolay_git@rambler.ru>
2022-06-26 13:27:15 +0500

    Программа проверки пользовательского числа на одновременную кратность 7 и 23

---
+++ Ceminar_2/005_Checking_for_multiplicity_of_7_and_23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int divider1 = 7,
+    divider2 = 23;
+if (userNumber%divider1 ==0 && userNumber%divider2 == 0)
+{
+    Console.WriteLine($"{userNumber} -> да"); //14 -> нет ,46 -> нет, 161 -> да
+}
+else
+{
+    Console.WriteLine($"{userNumber} -> нет");
+}

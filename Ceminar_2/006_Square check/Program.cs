00d4f459462851b526bd9141c003ef4d82693e6b
Nikolay <nikolay_git@rambler.ru>
2022-06-26 13:39:38 +0500

    Добавлен более короткий вариант программы

--- Ceminar_2/006_Square check/Program.cs
+++ Ceminar_2/006_Square check/Program.cs
@@ -3,11 +3,9 @@ int userNumber1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число: ");
 int userNumber2 = Convert.ToInt32(Console.ReadLine());
 
-if (userNumber1 > userNumber2 && userNumber1 == userNumber2*userNumber2)
-{
-    Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
-}
-else if (userNumber1 < userNumber2 && userNumber2 == userNumber1*userNumber1)
+// Вариант 3
+
+if (userNumber1 = userNumber2 * userNumber2 || userNumber2 = userNumber1 * userNumber1)
 {
     Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
 }
@@ -17,6 +15,25 @@ else
 }
 
 
+
+
+// Вариант 2
+
+// if (userNumber1 > userNumber2 && userNumber1 == userNumber2*userNumber2)
+// {
+//     Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
+// }
+// else if (userNumber1 < userNumber2 && userNumber2 == userNumber1*userNumber1)
+// {
+//     Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
+// }
+// else
+// {
+//     Console.WriteLine($"{userNumber1}, {userNumber2} -> нет");
+// }
+
+
+// Вариант 1
 
 //     // if (userNumber1 == userNumber2*userNumber2)
 //     // {

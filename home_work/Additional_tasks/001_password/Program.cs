0c5e2aca100ed9f2fcd06f44801edd77bae94980
Nikolay <nikolay_git@rambler.ru>
2022-06-26 19:10:45 +0500

    Доп задание №1

---
+++ home_work/Additional_tasks/001_password/Program.cs
@@ -0,0 +1,29 @@
+﻿string secret_message = "Пельмени надо варить 2 минуты после всплытия",
+    password = "password";
+
+int count = 0,
+    max_attemp = 3;
+
+Console.WriteLine($"У вас будет {max_attemp} попытки чтобы ввести пароль.");
+
+while (count < max_attemp)
+{
+    Console.Write($"Попытка №{count + 1}: ");
+    string userPassword = Console.ReadLine();
+
+    if (userPassword == password)
+    {
+        Console.WriteLine($"Секретное сообщение: {secret_message}");
+        break; // выход из цикла, завершение программы.
+    }    
+   else
+   {
+        Console.WriteLine("Неверный пароль");
+        count++;
+   }
+}
+
+if (count == max_attemp)
+{
+    Console.WriteLine("Вы исчерпали все попытки.");
+}

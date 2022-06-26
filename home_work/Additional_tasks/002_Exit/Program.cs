dc32e412b4261fcd5b0a698b149e8cc3277ea747
Nikolay <nikolay_git@rambler.ru>
2022-06-26 19:23:54 +0500

    Доп задание №2

---
+++ home_work/Additional_tasks/002_Exit/Program.cs
@@ -0,0 +1,18 @@
+﻿string exit = "exit";
+int count = 1;
+Console.WriteLine("Программа работает исправно");
+while (true)
+{
+    Console.Write("Если хотите завершить программу введите exit: ");
+    string userCommand = Console.ReadLine();
+
+    if (userCommand.ToLower() == exit)
+    {
+        Console.WriteLine("Программа закрыта");
+        break;
+    } 
+    else
+    {
+        Console.WriteLine($"{count++} цикл");
+    }
+}

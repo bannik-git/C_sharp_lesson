41e29fff00341251ea6193607902ebc97a8e5590
Nikolay <nikolay_git@rambler.ru>
2022-06-26 12:58:25 +0500

    Программа нахождения максимального числа (78 -> 8)

---
+++ Ceminar_2/001_source_max_number/Program.cs
@@ -0,0 +1,14 @@
+﻿int randomNumber = new Random().Next(10, 100);
+
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+
+int max = firstDigit;
+
+if (secondDigit > max)
+{
+    max = secondDigit;
+}
+
+Console.WriteLine($"Число = {randomNumber}, макс цифра = {max}");
+

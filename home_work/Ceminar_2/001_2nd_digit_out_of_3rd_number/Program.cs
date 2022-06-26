9dd214dffb9feef2617f9067655d2fbc0e15dcfd
Nikolay <nikolay_git@rambler.ru>
2022-06-26 18:34:09 +0500

    Программа задания 1 семинар 2

---
+++ home_work/Ceminar_2/001_2nd_digit_out_of_3rd_number/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int number2nd = userNumber % 100 / 10;
+
+Console.WriteLine($"{number2nd} - вторая цифра в числе {userNumber}");

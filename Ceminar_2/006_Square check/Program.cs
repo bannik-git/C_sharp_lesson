17d59e0c21043fa8304ce54874374fa3e974b036
Nikolay <nikolay_git@rambler.ru>
2022-06-26 13:28:58 +0500

    Программа проверяющая является ли одно из чисел квадратом другого

---
+++ Ceminar_2/006_Square check/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber1 > userNumber2 && userNumber1 == userNumber2*userNumber2)
+{
+    Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
+}
+else if (userNumber1 < userNumber2 && userNumber2 == userNumber1*userNumber1)
+{
+    Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
+}
+else
+{
+    Console.WriteLine($"{userNumber1}, {userNumber2} -> нет");
+}
+
+
+
+//     // if (userNumber1 == userNumber2*userNumber2)
+//     // {
+//         Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{userNumber1}, {userNumber2} -> нет");
+//     }
+// }
+// else
+// {
+//     if (userNumber2 == userNumber1*userNumber1)
+//     {
+//         Console.WriteLine($"{userNumber1}, {userNumber2} -> да");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{userNumber1}, {userNumber2} -> нет");
+//     }
+// }
+

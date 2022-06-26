3882f6a58fc3efba9e77ae7c8f0d36c269b13051
Nikolay <nikolay_git@rambler.ru>
2022-06-26 13:01:53 +0500

    Создание программы выводящей из трехзначного числа первую и последнюю цифры

---
+++ Ceminar_2/003_Remove_number_in_the_middle/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.Write("Введите трех значное число: ");
+// int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int randomNumber = new Random().Next(100, 1000);
+
+Console.WriteLine(randomNumber);
+
+int number1 = randomNumber / 100;
+int number3 = randomNumber % 10;
+
+// Если нужно число:
+// int result = number1*10 + number3;
+// Console.WriteLine(result);
+
+Console.WriteLine($"{number1}{number3}");
+
+

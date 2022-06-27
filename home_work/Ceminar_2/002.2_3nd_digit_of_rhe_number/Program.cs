a009235892f93ac6426b53a5b0010b5d58a6e0fb
Nikolay <nikolay_git@rambler.ru>
2022-06-27 18:53:08 +0500

    Изменение стиля названия переменных

--- home_work/Ceminar_2/002.2_3nd_digit_of_rhe_number/Program.cs
+++ home_work/Ceminar_2/002.2_3nd_digit_of_rhe_number/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write("Введите число: ");
 
-string userNumber_string = Console.ReadLine();
-int userNumber = Convert.ToInt32(userNumber_string);
-int size = userNumber_string.Length; // находим длину строки, понимаем скольки значное число
+string userNumberString = Console.ReadLine();
+int userNumber = Convert.ToInt32(userNumberString);
+int size = userNumberString.Length; // находим длину строки, понимаем скольки значное число
 
 if (userNumber < 0)
 {
@@ -16,21 +16,21 @@ if (size <= 2) // если size <= 2 число может быть максим
 else
 {
     int count = 0,
-        number_for_remainder = 1, // число для нахождения остатка 
+        numberForRemainder = 1, // число для нахождения остатка 
         divider = 1; // число на которое будет делится остаток
 
     while (count < size - 2) // size - 2 для начала сразу с 3х значных чисел.
     {
         if (count == 0)
         {
-            number_for_remainder *= 10;
+            numberForRemainder *= 10;
         }
         else
         {
-            number_for_remainder *= 10;
+            numberForRemainder *= 10;
             divider *= 10;
         }
         count++;
     }
-    Console.WriteLine(Math.Abs(userNumber % number_for_remainder / divider));
+    Console.WriteLine(Math.Abs(userNumber % numberForRemainder / divider));
 }

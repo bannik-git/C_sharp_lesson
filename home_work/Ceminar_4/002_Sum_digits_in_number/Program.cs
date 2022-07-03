f6978c37d3570d5aef7f4764529ca244d300e702
Nikolay <nikolay_git@rambler.ru>
2022-07-03 17:54:48 +0500

    Задача по нахождению суммы цифр в числе

---
+++ home_work/Ceminar_4/002_Sum_digits_in_number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write($"Введите число от 1 до {int.MaxValue}: ");
+string number = Console.ReadLine() ?? "";
+int userNumber = Convert.ToInt32(number),
+    result = 0;
+
+for (int count = 0; count < number.Length; count++)
+{
+    result += userNumber % 10;
+    userNumber /= 10;
+}
+
+Console.WriteLine(result);

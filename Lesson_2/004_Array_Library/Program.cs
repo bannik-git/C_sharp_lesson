ec7ca68b62cddf4a88210a65dc2aa55a3c42b862
Nikolay <nikolay_git@rambler.ru>
2022-07-03 20:23:37 +0500

    Пример работы с массивами с использованием методов

---
+++ Lesson_2/004_Array_Library/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[] collection) // void - метод кторый ничего не возвращает
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {   
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) 
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);

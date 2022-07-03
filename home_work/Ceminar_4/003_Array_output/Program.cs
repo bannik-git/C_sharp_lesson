12a282f68a44053e178aa626af76589244a4ab0e
Nikolay <nikolay_git@rambler.ru>
2022-07-03 18:47:46 +0500

    Добавлена возможность выбора вида массива и косметические изменения вывода результата в консоль

--- home_work/Ceminar_4/003_Array_output/Program.cs
+++ home_work/Ceminar_4/003_Array_output/Program.cs
@@ -1,11 +1,38 @@
-﻿int[] GetRandomArray (int arraySize, int fromNumber, int upToNumber)
+﻿using System.Threading;
+
+int[] GetRandomArray ()
 {
+    Console.Clear();
+    Console.WriteLine("Инициировано создание массива состоящего из случайных значений.");
+    Console.Write(" Шаг 1. Введите нужную длину массива: ");
+    int arraySize = Convert.ToInt32(Console.ReadLine());
+    Console.Write(" Шаг 2. Введите начало диапазона случайных чисел: ");
+    int fromNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write(" Шаг 3. Введите конец диапазона случайных чисел: ");
+    int upToNumber = Convert.ToInt32(Console.ReadLine());
     int[] randomArray = new int [arraySize];
     for (int count = 0; count < arraySize; count++)
     {
         randomArray[count] = new Random().Next(fromNumber, upToNumber);
     }
     return randomArray;
+}
+
+int[] GetUserArray ()
+{   
+    Console.Clear();
+    Console.WriteLine("Инициировано создание пользовательского массива.");
+    Console.Write(" Шаг 1. Введите нужную длину массива: ");
+    int arraySize = Convert.ToInt32(Console.ReadLine());
+    Console.Write(" Шаг 2. Заполнение массив.");
+    Console.WriteLine();
+    int[] userArray = new int [Convert.ToInt32(arraySize)];
+    for (int count = 0; count < arraySize; count++)
+    {
+        Console.Write($"    Введите число №{count + 1}: ");
+        userArray[count] = Convert.ToInt32(Console.ReadLine());
+    }
+    return userArray;
 }
 
 void PrintArray (int[] array)
@@ -26,7 +53,34 @@ void PrintArray (int[] array)
             result += $"{array[count]}, ";
         }
     }
-    Console.Write(result);
+    Console.WriteLine("Подождите информация обрабатывается");
+    int bootStripeDelay = 50;
+    Console.Write("[");
+    for (int count = 0; count < 40; count++)
+    {
+        Console.Write("#");
+        Thread.Sleep(bootStripeDelay);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+    Console.Write(" Ваш массив - " + result);
 }
 
-PrintArray(GetRandomArray(8, 1, 10)); //8 - размер массива, 1 и 10 - диапазон чисел рамндомайзера. [1:10)
+Console.WriteLine("Какой массив вы хотите создать?");
+Console.WriteLine(" Массив из случайных значений в заданном диапазоне - цифра 1");
+Console.WriteLine(" Массив значния которого задаются пользователем -цифра 2");
+Console.Write("Введите цифру: ");
+int userAnswer = Convert.ToInt16(Console.ReadLine());
+
+if (userAnswer == 1)
+{
+    PrintArray(GetRandomArray());
+}
+else if (userAnswer == 2)
+{
+    PrintArray(GetUserArray());
+}
+else
+{
+    Console.WriteLine("Введено не допустимое значение");
+}

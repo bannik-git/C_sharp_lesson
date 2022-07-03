a0eee331a60e694de475583fcc707e3319c0f8a8
Nikolay <nikolay_git@rambler.ru>
2022-07-03 17:54:03 +0500

    Задача по возведению числа в степень

---
+++ home_work/Ceminar_4/001_Exponentiation/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNumber (string text)
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Exponentiation(int number, int degree)
+{
+    int result = number;
+    for (int count = 1; count < degree; count++)
+    {
+        result *= number;
+        // Console.WriteLine(number);
+    }
+    return result;
+}
+
+int userNumber = GetNumber("Ведите число: ");
+int degree = GetNumber("Введите степень: ");
+
+int result = Exponentiation(userNumber, degree);
+
+Console.WriteLine(result);

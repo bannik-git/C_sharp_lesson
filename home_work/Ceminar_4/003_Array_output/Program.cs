7bf99ad2b55b689e537a74748bcfa795f68e1edd
Nikolay <nikolay_git@rambler.ru>
2022-07-03 17:56:17 +0500

    Задача по созданию массива из 8 элементов и вывода их на экран

---
+++ home_work/Ceminar_4/003_Array_output/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] GetRandomArray (int arraySize, int fromNumber, int upToNumber)
+{
+    int[] randomArray = new int [arraySize];
+    for (int count = 0; count < arraySize; count++)
+    {
+        randomArray[count] = new Random().Next(fromNumber, upToNumber);
+    }
+    return randomArray;
+}
+
+void PrintArray (int[] array)
+{
+    string result = String.Empty; // Пустая строка
+    for (int count = 0; count < array.Length; count++)
+    {
+        if (count == 0)
+        {
+            result += $"[{array[count]}, ";
+        }
+        else if (count == array.Length - 1)
+        {
+            result += $"{array[count]}]";
+        }
+        else
+        {
+            result += $"{array[count]}, ";
+        }
+    }
+    Console.Write(result);
+}
+
+PrintArray(GetRandomArray(8, 1, 10)); //8 - размер массива, 1 и 10 - диапазон чисел рамндомайзера. [1:10)

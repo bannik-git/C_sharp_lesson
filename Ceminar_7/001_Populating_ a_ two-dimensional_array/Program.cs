7b79ca7370d0bd05f08140b41d590674e7fa6682
Nikolay <nikolay_git@rambler.ru>
2022-07-16 13:57:29 +0500

    Задачи Семинара №7

---
+++ Ceminar_7/001_Populating_ a_ two-dimensional_array/Program.cs
@@ -0,0 +1,38 @@
+﻿int GetNumber (string userString)
+{
+    Console.Write(userString);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray (int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write(array[m, n] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = GetNumber("Введите количество строк массива: "),
+    column = GetNumber("Введите количество столбцов массива: ");
+
+int[,] array = new int [row, column];
+
+// {   index  0  1  2
+//       0 - {1, 2, 3}
+//       1 - {4, 5, 6},
+//       2 - {7, 8, 9}
+// }
+
+for (int m = 0; m < array.GetLength(0); m++)
+{
+    for (int n = 0; n < array.GetLength(1); n++)
+    {
+        array [m, n] = m + n;
+    }
+}
+
+PrintArray(array);
---
+++ Ceminar_7/002_Odd_indices_squared/Program.cs
@@ -0,0 +1,50 @@
+﻿int GetNumber (string userString)
+{
+    Console.Write(userString);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray (int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write(array[m, n] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = GetNumber("Введите количество строк массива: "),
+    column = GetNumber("Введите количество столбцов массива: ");
+
+int[,] array = new int [row, column];
+
+// {   index  0  1  2
+//       0 - {1, 2, 3}
+//       1 - {4, 5, 6},
+//       2 - {7, 8, 9}
+// }
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array [i, j] = new Random().Next(0, 10);
+    }
+}
+
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if(i % 2 != 0 && j % 2 != 0)
+            array [i, j] *= array [i, j];
+    }
+}
+
+Console.WriteLine();
+PrintArray(array);
+

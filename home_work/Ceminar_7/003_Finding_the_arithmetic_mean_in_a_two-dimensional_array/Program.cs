281da0640a4821f1a83b41f62e4462854ed9aa14
Nikolay <nikolay_git@rambler.ru>
2022-07-16 16:12:43 +0500

    Задача по нахождению в двумерном массиве среднее арифметическое элементов в каждом столбце

---
+++ home_work/Ceminar_7/003_Finding_the_arithmetic_mean_in_a_two-dimensional_array/Program.cs
@@ -0,0 +1,55 @@
+﻿int GetNumber (string userString)
+{
+    Console.Write(userString);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray (int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write(array[m, n] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillArray (int row, int column)
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return array;
+}
+void PrintArifmeticMeanColumns (int[,] array)
+{   
+    int result = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            result += array[j, i];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {i + 1} = {(result * 1.0) / array.GetLength(0)}");
+        result = 0;
+    }
+}
+
+
+int row = GetNumber("Введите количество строк массива: "),
+    column = GetNumber("Введите количество столбцов массива: ");
+
+int[,] array = FillArray(row, column);
+PrintArray(array);
+Console.WriteLine();
+PrintArifmeticMeanColumns(array);
+
+

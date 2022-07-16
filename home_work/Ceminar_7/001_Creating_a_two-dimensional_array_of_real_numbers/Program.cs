a20890cc4a4de49386ddc87a12e61616a4f9f9c6
Nikolay <nikolay_git@rambler.ru>
2022-07-16 16:09:11 +0500

    Задача на заполнение двумерного массива случайными вещественными числами

---
+++ home_work/Ceminar_7/001_Creating_a_two-dimensional_array_of_real_numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿int GetNumber (string userString)
+{
+    Console.Write(userString);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray (double[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write(array[m, n] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] FillArray (int row, int column)
+{
+    double[,] array = new double[row, column];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(-10, 11) + new Random().NextDouble(), 4);
+        }
+    }
+    return array;
+}
+
+int row = GetNumber("Введите количество строк массива: "),
+    column = GetNumber("Введите количество столбцов массива: ");
+
+double[,] array = FillArray(row, column);
+PrintArray(array);

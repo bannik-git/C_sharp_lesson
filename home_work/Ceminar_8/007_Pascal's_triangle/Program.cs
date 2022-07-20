21a4a088e3b1fb587f9b332083b6a1a4725c50e1
Nikolay <nikolay_git@rambler.ru>
2022-07-20 20:45:57 +0500

    Задача на построение треугольника Паскаля

---
+++ home_work/Ceminar_8/007_Pascal's_triangle/Program.cs
@@ -0,0 +1,65 @@
+﻿int GetNumber (string userString)
+{
+    Console.Write(userString);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillArray (int[,] array)
+{
+    int m = 1,
+        n = array.GetLength(0);
+    for (int i  = 0; i < array.GetLength(0); i++)
+    {   
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j < n - 1 - i || j > n - 1 + i)
+                array[i, j] = 0;
+            else if (j == n - 1 - i || j == n - 1 + i)
+                array[i, j] = 1;
+            else
+            {
+                if (j == n - 1 - i + m * 2)
+                {
+                    array[i, j] = array[i - 1, j - 1] + array[i -1 , j + 1];
+                    m++;
+                }
+                else
+                    array[i, j] = 0;                
+            }
+        }
+        m = 1;
+    }
+    return array;
+}
+
+void PrintArray (int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == 0)
+                Console.Write("     ");
+            else
+                if (array[i, j] < 10)
+                    Console.Write($"  {array[i, j]}  ");
+                else if (array[i, j] < 100)
+                    Console.Write($"  {array[i, j]} ");
+                else if (array[i, j] < 1000)
+                    Console.Write($" {array[i, j]} ");
+                else
+                    Console.Write($"{array[i, j]} ");
+        }       
+    Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int numberString = GetNumber("Введите нормер строки треугольника: ");
+int[,] pascalTriangle = new int[numberString, numberString * 2 -1];
+
+pascalTriangle = FillArray(pascalTriangle);
+PrintArray(pascalTriangle);
+
+

91a6fee4424c8316a0f44ece0b999023cb01e995
Nikolay <nikolay_git@rambler.ru>
2022-07-24 20:36:59 +0500

    Задача написать программу вычисляющую фунцию Аккермана для двух не отрицательных чисел

---
+++ home_work/Ceminar_9 (recursion)/003_calculation_of_the_Ackermann_function/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetNumber (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AckermannFunction (int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return AckermannFunction(m - 1, 1);
+    else
+        return AckermannFunction(m - 1, AckermannFunction(m, n-1)); 
+}
+
+int number1 = GetNumber("Введите число №1: "),
+    number2 = GetNumber("Введите число №2: ");
+
+Console.Write(AckermannFunction(number1, number2));

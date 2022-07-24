9764e735a42b9a6ea1029dc2f1cbe9f421d3bb26
Nikolay <nikolay_git@rambler.ru>
2022-07-24 20:34:33 +0500

    Задача вывести натуральные числа от N до M

---
+++ home_work/Ceminar_9 (recursion)/001_Natural_numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿int GetNumber (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PrintNumber (int number1, int number2)
+{
+    int max = number1,
+        min = number2;    
+    if (min > max)
+    {
+        max = number2;
+        min = number1;
+    }
+    if (min != max)
+    {
+        Console.Write(PrintNumber(max - 1, min) + ", ");
+        return max;
+    }
+    return min;
+}
+
+int number1 = GetNumber("Введите число №1: "),
+    number2 = GetNumber("Введите число №2: ");
+
+Console.Write(PrintNumber(number1, number2));

d96d6a0b750ec86a7e5a7b4332c98f00f1878748
Nikolay <nikolay_git@rambler.ru>
2022-07-24 20:35:32 +0500

    Задача найти сумму натуральных чисел между N и M

---
+++ home_work/Ceminar_9 (recursion)/002_sum_of_natural_numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNumber (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PrintNumber (int number1, int number2)
+{
+    int max = number1,
+        min = number2;    
+    if (min > max)
+    {
+        max = number2;
+        min = number1;
+    }
+    if (min != max)
+        return PrintNumber(max - 1, min) + max;
+    return min;
+}
+
+int number1 = GetNumber("Введите число №1: "),
+    number2 = GetNumber("Введите число №2: ");
+
+Console.Write(PrintNumber(number1, number2));

2da1e31dbbcfaf8c7eb1a5144614c7a0687de055
Nikolay <nikolay_git@rambler.ru>
2022-09-05 21:23:19 +0500

    Pac-Man_v1.0

--- home_work/Additional_tasks/008_RPG_game/Program.cs
+++ home_work/Additional_tasks/008_RPG_game/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
-using System.Timers;
+
+using System.IO;
 
 string GetAnswer (string text)
 {
@@ -13,7 +14,10 @@ void PrintArray (string[] array)
             Console.WriteLine(array[i]);
 }
 
-void PrintField(string[,] field, int[] userCoordinates, string instruction, char userSymbol)
+void PrintField(string[,] field, 
+                int[] userCoordinates, char userSymbol, 
+                int[,] enemyCoordinates, char enemySymbol,
+                string instruction)
 {
     Console.Clear();
     for (int i = 0; i < field.GetLength(0); i++)
@@ -25,8 +29,15 @@ void PrintField(string[,] field, int[] userCoordinates, string instruction, char
         Console.WriteLine();
     }
     Console.WriteLine(instruction + "\n");
-    Console.SetCursorPosition(userCoordinates[2], userCoordinates[0]);
-    Console.Write('@');
+    Console.SetCursorPosition(userCoordinates[1], userCoordinates[0]);
+    Console.Write(userSymbol);
+
+    for (int i = 0; i < enemyCoordinates.GetLength(0); i++)
+    {
+        Console.SetCursorPosition(enemyCoordinates[i, 1], enemyCoordinates[i, 0]);
+        Console.Write(enemySymbol); 
+    }
+    
 }
 
 void MapFormation (string[,] field, string path)
@@ -36,7 +47,7 @@ void MapFormation (string[,] field, string path)
         field[i, 0] = map.ReadLine() ?? String.Empty;
 }
 
-void GetPlaceOfAppearance (string[,] field, int[] userCoorinates)
+void GetPlaceOfAppearance (string[,] field, int[] userCoorinates, int[] Coorinates = null)
 {
     Random random = new Random();
     int i = 0,
@@ -45,14 +56,48 @@ void GetPlaceOfAppearance (string[,] field, int[] userCoorinates)
     {
         i = random.Next(0, field.GetLength(0));
         j = random.Next(0, field[0,0].Length);
-        if(field[i, 0][j] != '*')
+        if (Coorinates != null)
+        {
+            if(field[i, 0][j] != '*' && i != userCoorinates[0] && j != userCoorinates[1])
+            {
+                Coorinates[0] = i;
+                Coorinates[1] = j;
+                return;
+            }
+        }
+        else if (field[i, 0][j] != '*')
         {
             userCoorinates[0] = i;
-            userCoorinates[2] = j;
+            userCoorinates[1] = j;
             return;
+        }
+        
+    }
+}
+
+void GetPlaceOfAppearanceEnemy (string[,] field, int[] userCoorinates, int[,] enemyCoorinates)
+{
+    Random random = new Random();
+    int i = 0,
+        j = 0;
+
+    for (int k = 0; k < enemyCoorinates.GetLength(0); k++)      
+    {
+        bool check = true;
+        while(check)
+        {
+            i = random.Next(0, field.GetLength(0));
+            j = random.Next(0, field[0,0].Length);
+            if(field[i, 0][j] != '*' && i != userCoorinates[0] && j != userCoorinates[1])
+            {
+                enemyCoorinates[k, 0] = i;
+                enemyCoorinates[k, 1] = j;
+                break;
+            }
         }
     }
 }
+
 
 void Moving(string[,] field, int[] userCoordinates, ConsoleKey key)
 {
@@ -60,29 +105,106 @@ void Moving(string[,] field, int[] userCoordinates, ConsoleKey key)
     {
         case ConsoleKey.UpArrow:
         {
-            if (field[userCoordinates[0] - 1, 0][userCoordinates[2]] != '*')
+            if (field[userCoordinates[0] - 1, 0][userCoordinates[1]] != '*')
                 userCoordinates[0]--;
             break;
         }
         case ConsoleKey.DownArrow:
         {
-            if (field[userCoordinates[0] + 1, 0][userCoordinates[2]] != '*')
+            if (field[userCoordinates[0] + 1, 0][userCoordinates[1]] != '*')
                 userCoordinates[0]++;   
             break;
         }
         case ConsoleKey.LeftArrow:
         {
-            if (field[userCoordinates[0], 0][userCoordinates[2] - 1] != '*')
-                userCoordinates[2]--;
+            if (field[userCoordinates[0], 0][userCoordinates[1] - 1] != '*')
+                userCoordinates[1]--;
             break;
         }
         case ConsoleKey.RightArrow:
         {
-            if (field[userCoordinates[0], 0][userCoordinates[2] + 1] != '*')
-                userCoordinates[2]++;
+            if (field[userCoordinates[0], 0][userCoordinates[1] + 1] != '*')
+                userCoordinates[1]++;
             break;
         }
     }   
+}
+
+void MovingEnemy(string[,] field, int[,] enemyCoordinates, 
+                Random direction, int directionEnemy, 
+                int stepEnemy, ref int stepCount)
+{
+    for (int i = 0; i < enemyCoordinates.GetLength(0); i++)
+    {
+        if (stepEnemy == stepCount)
+        {
+            directionEnemy = direction.Next(1,5);
+            stepCount = 0;
+        }
+        bool step = true;
+        while (step)
+        { 
+            switch(directionEnemy) 
+            {
+                case 1:
+                {
+                    if (field[enemyCoordinates[i, 0] - 1, 0][enemyCoordinates[i, 1]] != '*')
+                        enemyCoordinates[i, 0]--; 
+                    step = false;
+                    break;
+                }
+                case 2:
+                {
+                    if (field[enemyCoordinates[i, 0] + 1, 0][enemyCoordinates[i, 1]] != '*')
+                        enemyCoordinates[i, 0]++;  
+                    step = false;
+                    break;
+                }
+                case 3:
+                {
+                    if (field[enemyCoordinates[i, 0], 0][enemyCoordinates[i, 1] - 1] != '*')
+                        enemyCoordinates[i, 1]--;
+                    step = false;
+                    break;
+                }
+                case 4:
+                {
+                    if (field[enemyCoordinates[i, 0], 0][enemyCoordinates[i, 1] + 1] != '*')
+                        enemyCoordinates[i, 1]++;
+                    step = false;
+                    break;
+                }
+            } 
+        }
+    }  
+}
+
+bool Intersection (int[] userCoordinate, int[,] enemyCoordinates)
+{
+    bool result = false;
+    for (int i = 0; i < enemyCoordinates.GetLength(0); i++)
+    {
+        if (userCoordinate[0] == enemyCoordinates[i, 0] && userCoordinate[1] == enemyCoordinates[i, 1])
+            result = true;
+    }
+    return result;
+}
+
+bool Victory(string[,] field)
+{
+    bool result = true;
+    for (int i = 0; i < field.GetLength(0); i++)
+    {
+        foreach (char item in field[i, 0])
+        {
+            if ('.' == item)
+            {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
 }
 
 int NumberOfFiles = new DirectoryInfo(@$"{Directory.GetCurrentDirectory()}\Maps\").GetFiles().Length;
@@ -91,11 +213,19 @@ string path = @$"{Directory.GetCurrentDirectory()}\Maps\{level}_level.txt";
 int count = File.ReadAllLines(path).Length;
 string[,] field = new string[count, 1]; 
 MapFormation(field, path);
-char user = '@';
+
+char user = '@',
+    enemy = 'X';
+
 string instruction = "Для передвижения используйте стрелочки, для выхода Esc";
-int[] userCoordinates = new int[3];
+int[] userCoordinates = new int[2];
+
+int[,] enemyCoordinates = new int[3 ,2]; // 3 - Количество врагов
+
 
 GetPlaceOfAppearance(field, userCoordinates);
+GetPlaceOfAppearanceEnemy(field, userCoordinates, enemyCoordinates);
+
 ConsoleKeyInfo directionOfTravel;
 Console.CursorVisible = false;
 
@@ -104,17 +234,26 @@ string[] hello = {"Вас приветствует игра по поиску в
                   "Ваше местоположения обозначено знаком - @, * обозначены стены лабиринта",
                   "Для начала нажмите Enter"
                  };
+                 
 PrintArray(hello);
 Console.ReadKey();
+Random direction = new Random();
+int directionEnemy = direction.Next(1,5);
+Random step = new Random();
+int stepEnemy = step.Next(1, 4);
+int stepCount = 1;
 
 while (true)
 {
     
-    PrintField(field, userCoordinates, instruction, user);
+    PrintField(field, 
+               userCoordinates, user,
+               enemyCoordinates, enemy,
+               instruction);
+    Console.WriteLine(stepCount);
     directionOfTravel = Console.ReadKey();
     switch (directionOfTravel.Key)
     {
-
         case ConsoleKey.Escape:
         {
             Console.SetCursorPosition(0, field.GetLength(0) + 4);
@@ -126,15 +265,32 @@ while (true)
                 return;
             }
             else 
+                MapFormation(field, path);
                 GetPlaceOfAppearance(field, userCoordinates);
+                GetPlaceOfAppearanceEnemy(field, userCoordinates, enemyCoordinates);
             break;
         }
         default:
         {
+            if (Victory(field))
+            {
+                Console.SetCursorPosition(0, field.GetLength(0) + 2);
+                Console.WriteLine("Вы победили!");
+                goto case ConsoleKey.Escape;
+            }
+            if (Intersection(userCoordinates, enemyCoordinates))
+            {
+                Console.SetCursorPosition(0, field.GetLength(0) + 2);
+                Console.WriteLine("Game Over");
+                goto case ConsoleKey.Escape;
+            } 
             if (userCoordinates[0] != 0 && userCoordinates[0] != field.GetLength(0) - 1)
             {
-
+                field[userCoordinates[0], 0] = field[userCoordinates[0], 0].Remove(userCoordinates[1], 1).Insert(userCoordinates[1], ' '.ToString());
                 Moving(field, userCoordinates, directionOfTravel.Key);
+                MovingEnemy(field, enemyCoordinates, direction ,directionEnemy, stepEnemy, ref stepCount);
+                
+                stepCount++;
                 break;
             }
             else
